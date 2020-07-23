b26294521f1d09de85b8457fd4546bc354bb277b
Harsh <mjkgoa@gmail.com>
2020-07-23 12:34:51 +0530

    fix: revert eval

--- src/util/offsetKernel.ts
+++ src/util/offsetKernel.ts
@@ -1,4 +1,4 @@
-export default function offsetKernel(kernelFunc: Function | string): Function {
+export default function offsetKernel(kernelFunc: Function | string): any {
   const kernelFuncStrArr = kernelFunc.toString().split('\n'); // Array of each line in the string format of a kernel function
   kernelFuncStrArr.splice(
     1, // The second line or 1st index
@@ -6,10 +6,8 @@ export default function offsetKernel(kernelFunc: Function | string): Function {
     `this.thread.x = this.thread.x + this.constants.hive_offset_x`
   )
 
-  return eval( // string to function
-    kernelFuncStrArr.join('\n') // back to string
+  return kernelFuncStrArr.join('\n') // back to string
     .replace(/this\.output\.x/g, 'this.constants.hive_output_x') // Individual kernel output != overall hive output
     .replace(/this\.output\.y/g, 'this.constants.hive_output_y')
     .replace(/this\.output\.z/g, 'this.constants.hive_output_z')
-  )
 }
