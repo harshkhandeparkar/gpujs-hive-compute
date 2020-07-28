dc3bbc7b531d8b9de76f917a604c863b669f29a7
HarshKhandeparkar <mjkgoa@gmail.com>
2020-07-29 01:33:04 +0530

    fix: offsetKernel supports 2-D and 3-D fully

--- src/util/offsetKernel.ts
+++ src/util/offsetKernel.ts
@@ -1,9 +1,11 @@
-export default function offsetKernel(kernelFunc: Function | string): any {
+export default function offsetKernel(kernelFunc: Function | string, outputDimensions: number): any {
   const kernelFuncStrArr = kernelFunc.toString().split('\n'); // Array of each line in the string format of a kernel function
   kernelFuncStrArr.splice(
     1, // The second line or 1st index
     0, // Insert an element instead of deleting
-    `this.thread.x = this.thread.x + this.constants.hive_offset_x`
+    `this.thread.x = this.thread.x + this.constants.hive_offset_x`,
+    outputDimensions > 1 ? `this.thread.y = this.thread.y + this.constants.hive_offset_y` : ``,
+    outputDimensions > 2 ? `this.thread.z = this.thread.z + this.constants.hive_offset_z` : ``
   )
 
   return kernelFuncStrArr.join('\n') // back to string
