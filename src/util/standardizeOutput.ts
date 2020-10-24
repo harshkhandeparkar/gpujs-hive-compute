c63980293c661098f95057d309ed461eb0bd68c7
HarshKhandeparkar <mjkgoa@gmail.com>
2020-10-25 00:05:30 +0530

    fix: make standardizeOutput types strict

--- src/util/standardizeOutput.ts
+++ src/util/standardizeOutput.ts
@@ -1,18 +1,21 @@
 import { IKernelXYZ } from 'gpu.js'
 
-export default function standardizeOutput(output: IKernelXYZ | number[]) {
-  let finalOutput: any;
+export default function standardizeOutput(output: IKernelXYZ | [number, number?, number?]) {
+  let finalOutput: [
+    number,
+    number?,
+    number?
+  ]
 
   if (Object.getOwnPropertyNames(output).includes('x')) {
     output = output as IKernelXYZ;
     // Convert object to array
-    finalOutput = [];
+    finalOutput = [output.x];
 
-    if (output.x) finalOutput.push(output.x);
     if (output.y) finalOutput.push(output.y);
     if (output.z) finalOutput.push(output.z);
   }
-  else finalOutput = output;
+  else finalOutput = <[number, number?, number?]>output;
 
   return {
     finalOutput,
