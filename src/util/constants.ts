70207120938a85b232872a583754aa79656fa513
HarshKhandeparkar <mjkgoa@gmail.com>
2020-10-24 13:20:39 +0530

    fix: convert enums to named enums

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,4 +1,12 @@
-export enum COMM_TYPE { ASK, TELL, REQUEST_CONN };
+export enum COMM_TYPE {
+  ASK = 'ask',
+  TELL = 'tell',
+  REQUEST_CONN = 'request_conn'
+}
 
-export enum TELL_ACTIONS { KERNEL_BUILT, KERNEL_RUN_DONE, CONN_ACCEPTED };
-export enum ASK_ACTIONS { BUILD_KERNEL, RUN_KERNEL };
+export enum TELL_ACTIONS {
+  KERNEL_BUILT = 'kernel_built',
+  KERNEL_RUN_DONE = 'kernel_run_done',
+  CONN_ACCEPTED = 'conn_accepted'
+}
+export enum ASK_ACTIONS { BUILD_KERNEL, RUN_KERNEL }
