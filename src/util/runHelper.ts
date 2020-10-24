ac6ca7251e0f4657535c09bcd84aa1bfb89eea2d
HarshKhandeparkar <mjkgoa@gmail.com>
2020-10-24 13:27:09 +0530

    fix: convert helper to a promise

--- src/util/runHelper.ts
+++ src/util/runHelper.ts
@@ -5,46 +5,55 @@ import { ASK_DATA } from './types';
 import { onAsk, tell, onTell } from './comm';
 
 /**
- * 
+ *
  * @param gpu Instance of GPU.js `GPU` class
  * @param url WebSocket URL e.g: ws://localhost:4532
  * @param logFunction A custom log function
  */
-export function runHelper(WS, gpu: GPU, url: string, logFunction: Function = console.log) {
-  const ws = new WS(url);
-  let k: IKernelRunShortcut; // build kernel will be stored here
-  
-  ws.on('open', () => {
-    logFunction('Connecting as helper.');
-    ws.send(JSON.stringify({type: COMM_TYPE.REQUEST_CONN, data: {}}));
-  
-    onTell(ws, TELL_ACTIONS.CONN_ACCEPTED, () => logFunction('Connection Accepted.'));
-  })
-  
-  ws.on('close', () => logFunction(`Connection refused or closed unexpectedly.`))
-  
-  onAsk(ws, ASK_ACTIONS.BUILD_KERNEL, (data: ASK_DATA) => { // Build the kernel
-    logFunction('building');
-    k = gpu.createKernel(data.extras.kernelFunc, data.extras.kernelOptions); //  Build the kernel
-    logFunction('built');
-  
-    tell(ws, {
-      action: TELL_ACTIONS.KERNEL_BUILT
+export async function runHelper(WS: any, gpu: GPU, url: string, logFunction: Function = console.log) {
+  return new Promise((resolve: () => void, reject: (e: Error) => void) => {
+    const ws = new WS(url);
+    let k: IKernelRunShortcut; // build kernel will be stored here
+
+    ws.on('open', () => {
+      logFunction('Connecting as helper.');
+      ws.send(JSON.stringify({type: COMM_TYPE.REQUEST_CONN, data: {}}));
+
+      onTell(ws, TELL_ACTIONS.CONN_ACCEPTED, () => logFunction('Connection Accepted.'));
     })
-  })
-  
-  onAsk(ws, ASK_ACTIONS.RUN_KERNEL, (data: ASK_DATA) => { // Run the kernel
-    logFunction('running');
-    let output: KernelOutput;
-    if (data.extras.inputsLength > 0) output = k(...data.extras.inputs); //  Run the kernel
-    else output = k();
-    logFunction('done');
-  
-    tell(ws, {
-      action: TELL_ACTIONS.KERNEL_RUN_DONE,
-      extras: {
-        output: output
-      }
+
+    ws.on('error', () => {
+      reject(new Error('WebSocket error.'));
+    })
+
+    ws.on('close', () => {
+      logFunction('Connection closed.');
+      resolve();
+    })
+
+    onAsk(ws, ASK_ACTIONS.BUILD_KERNEL, (data: ASK_DATA) => { // Build the kernel
+      logFunction('building');
+      k = gpu.createKernel(data.extras.kernelFunc, data.extras.kernelOptions); //  Build the kernel
+      logFunction('built');
+
+      tell(ws, {
+        action: TELL_ACTIONS.KERNEL_BUILT
+      })
+    })
+
+    onAsk(ws, ASK_ACTIONS.RUN_KERNEL, (data: ASK_DATA) => { // Run the kernel
+      logFunction('running');
+      let output: KernelOutput;
+      if (data.extras.inputsLength > 0) output = k(...data.extras.inputs); //  Run the kernel
+      else output = k();
+      logFunction('done');
+
+      tell(ws, {
+        action: TELL_ACTIONS.KERNEL_RUN_DONE,
+        extras: {
+          output: output
+        }
+      })
     })
   })
 }
